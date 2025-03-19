a5be9f15bd85b766ff1c1c1bbafd76b4778bb620
BlackIce417 <2895341955@qq.com>
2025-03-19 23:54:23 +0800

    front: uplaod multi images

--- main/static/js/album.js
+++ main/static/js/album.js
@@ -89,18 +89,23 @@ $(document).ready(function () {
         $("#input-add-tag").show();
     });
 
-    $("#btn-upload-single-img").click(function(e) {
-        $("#upload-single-image").click(); 
+    $("#btn-upload-single-img").click(function (e) {
+        $("#upload-single-image").click();
     });
-    
-    $("#upload-single-image").change(function() {
+
+    $("#upload-single-image").change(function () {
         let file = this.files[0];
         if (file) {
             let reader = new FileReader();
             reader.readAsDataURL(file);
-            reader.onloadend = function() {
-                $("#img-item").attr("src", this.result);
-                $(".img-item").show();
+            reader.onloadend = function () {
+                // $("#img-item").attr("src", this.result);
+                // $(".img-item").show();
+                let $imgItemWrapper = createImageItemWrapper();
+                $(".upload-box").before($imgItemWrapper);
+                $imgItemWrapper.find("img").attr("src", this.result);
+                console.log($imgItemWrapper);
+                $imgItemWrapper.show();
             };
         }
     })
@@ -112,4 +117,38 @@ $(document).ready(function () {
 });
 function hidePopup(params) {
     $("#overlay").hide();
+}
+
+function createImageItemWrapper() {
+    let uniqueId = Date.now().toString(36) + Math.random().toString(36).slice(2, 9);
+    let $wrapper = $("<div>").addClass("img-item").attr("id", "img-wrapper-" + uniqueId).css({
+        "position": "relative",
+        "margin-bottom": "10px"
+    });
+    let $img = $("<img>").attr("src", "#").css({
+        "width": "100%",
+        "height": "125px",
+        "object-fit": "contain"
+    });
+    let $button = $("<button>", {
+        class: 'd-flex align-items-center justify-content-center',
+        id: 'btn-deleteimg',
+        type: 'button',
+        id: "btn-deleteImgItem-" + uniqueId,
+        css: {
+            height: '25px',
+            width: '100%',
+            border: 'none',
+            backgroundColor: 'transparent'
+        }
+    });
+    let $svg = $(`
+        <svg t="1742375343895" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2775" width="100%" height="80%">
+            <path d="M1001.702178 133.586954H22.297822C9.999024 133.586954 0 143.685968 0 155.884777c0 12.298799 9.999024 22.297822 22.297822 22.297822h111.189142v779.023924c0 36.796407 29.997071 66.793477 66.793477 66.793477h623.339127c36.796407 0 66.693487-29.997071 66.693487-66.793477V178.182599h111.289132c12.298799 0 22.297822-9.999024 22.397813-22.297822 0-12.298799-10.099014-22.297822-22.297822-22.297823zM845.917391 957.206523c0 12.298799-9.999024 22.297822-22.297823 22.297822H200.380432c-12.298799 0-22.297822-9.999024-22.297823-22.297822V187.08173H845.917391v770.124793zM378.36305 44.595645h267.173909c12.198809 0 22.197832-9.999024 22.197833-22.297823S657.635778 0 645.436969 0H378.36305c-12.298799 0-22.297822 10.099014-22.297822 22.297822 0 12.298799 10.099014 22.297822 22.297822 22.297823z" fill="" p-id="2776"></path>
+            <path d="M400.660873 667.834782c12.298799 0 22.297822-10.099014 22.297822-22.297823V378.463041c0-12.198809-10.099014-22.197832-22.297822-22.297823-12.298799 0-22.297822 10.099014-22.297823 22.297823v267.073918c0 12.298799 10.099014 22.297822 22.297823 22.297823zM623.239137 667.834782c12.298799 0 22.297822-10.099014 22.297822-22.297823V378.463041c0-12.198809-10.099014-22.197832-22.297822-22.297823-12.298799 0-22.297822 10.099014-22.297823 22.297823v267.073918c0 12.298799 10.099014 22.297822 22.297823 22.297823z" fill="" p-id="2777"></path>
+        </svg>
+        `);
+    $button.append($svg);
+    $wrapper.append($img, $button);
+    return $wrapper;
 }
